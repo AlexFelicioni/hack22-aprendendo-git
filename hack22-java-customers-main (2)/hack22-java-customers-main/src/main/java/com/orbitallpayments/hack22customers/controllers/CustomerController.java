259f4d12184cf6e1cd8c92da025de41bed323680
AlexFelicioni <alexfelicioni@gmail.com>
2022-02-12 09:37:14 -0300

    atualizacao

--- hack22-java-customers-main (2)/hack22-java-customers-main/src/main/java/com/orbitallpayments/hack22customers/controllers/CustomerController.java
+++ hack22-java-customers-main (2)/hack22-java-customers-main/src/main/java/com/orbitallpayments/hack22customers/controllers/CustomerController.java
@@ -1,28 +1,37 @@
 package com.orbitallpayments.hack22customers.controllers;
+
 import com.orbitallpayments.hack22customers.domains.Customer;
 import com.orbitallpayments.hack22customers.services.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 import java.util.Optional;
+
 @RestController
-@RequestMapping
-public class CustomerControler {
+@RequestMapping("/customers")
+public class CustomerController {
     @Autowired
-    private ResponseEntity<Customer> save(@RequestBody Customer customer){
-        return new ResponseEntity<Customer>(savedCustomer, HttpStatus.CREATED);
+    private CustomerService customerService;
+
+    @PostMapping
+    public ResponseEntity save(@RequestBody Customer customer) {
+        Customer savedCustomer = customerService.save(customer);
+
+        return new ResponseEntity(savedCustomer, HttpStatus.CREATED);
     }
 
     @GetMapping
     public ResponseEntity<List<Customer>> findAll() {
-        List<Customer> customers = customersService.findAll();
+        List<Customer> customers = customerService.findAll();
         return ResponseEntity.ok(customers);
     }
+
     @GetMapping("/{id}")
-    public ResponseEntity<Customer> findById(@PathVariable long ig){
-        Optional<Customer> fetchedCustomer = customerservice.findById(id);
+    public ResponseEntity<Customer> findById(@PathVariable Long id) {
+        Optional<Customer> fetchedCustomer = customerService.findById(id);
 
         return ResponseEntity.ok(fetchedCustomer.get());
     }
