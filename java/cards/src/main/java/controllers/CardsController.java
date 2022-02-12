afe09abe47b0cc4d58583dde9589447a5c0b7d9b
AlexFelicioni <alexfelicioni@gmail.com>
2022-02-12 12:10:32 -0300

    terceiro update

--- java/cards/src/main/java/controllers/CardsController.java
+++ java/cards/src/main/java/controllers/CardsController.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import repositories.CardsRepository;
 import services.CardsService;
-
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
